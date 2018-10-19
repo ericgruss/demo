a5a2ca6b0010f708aefcb3e6b44470f7dfd6f3b4
ericgruss <eric.gruss@gmail.com>
2018-10-19 10:13:06 -0400

    Update config.php

--- app/config.php
+++ app/config.php
@@ -7,10 +7,11 @@ use SuperBlog\Persistence\InMemoryArticleRepository;
 return [
     // Bind an interface to an implementation
     ArticleRepository::class => create(InMemoryArticleRepository::class),
-
+     
     // Configure Twig
     Twig_Environment::class => function () {
         $loader = new Twig_Loader_Filesystem(__DIR__ . '/../src/SuperBlog/Views');
+        $loader2 = new Twig_Loader_Filesystem(__DIR__ . '/../src/SuperBlog/Views');
         return new Twig_Environment($loader);
     },
 ];
